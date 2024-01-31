b83d6bf13beac4914c2bd2f9d10ffd5ea7bee152
KP <117683785+karnagetm@users.noreply.github.com>
2024-01-31 17:50:22 +0000

    V1.1
    Basic UI changes

---
+++ BlockchainAssignment/BlockchainAssignment/BlockchainApp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BlockchainAssignment
+{
+    public partial class BlockchainApp : Form
+    {
+        public BlockchainApp()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = textBox1.Text;
+        }
+    }
+}
